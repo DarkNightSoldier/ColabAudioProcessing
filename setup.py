470fd709d88f99015b4c27fce192fb3f25fa51ae
Alejandro Higuera C <51842714+DarkNightSoldier@users.noreply.github.com>
2020-07-06 20:21:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import find_packages, setup
+from distutils.core import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="colabaudiopr_Es",
-    packages=find_packages(),
-    version="1.1.0",
-    description="Analisis y modificacion .wav",
+    name="colabaudiopr_es",
+    packages=['colabaudiopr_es'],
+    version="1.0",
+    description="Implementación para el análisis y modificacion de archvos .wav en Google Colaboratory",
     author="Alejandro Higuera",
     license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/timsainb/noisereduce",
+    url="https://github.com/DarkNightSoldier/colabaudiopr_es,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
